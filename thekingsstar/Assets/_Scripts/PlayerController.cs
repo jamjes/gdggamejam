02765a10cd0bd87b6f68f215c245e2c570d2dd94
Jam <77445181+jamjes@users.noreply.github.com>
2024-04-06 17:25:31 +0100

    //Player Animation
    - Idle animation will run as the default.
    - Attack animation will run when the attack key is pressed.

--- thekingsstar/Assets/_Scripts/PlayerController.cs
+++ thekingsstar/Assets/_Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     BoxCollider2D _col;
     [SerializeField] LayerMask projectileLayer;
+    [SerializeField] Animator anim;
+    bool isAttacking = false;
+    static readonly int SlashAnimation = Animator.StringToHash("slash");
+    static readonly int IdleAnimation = Animator.StringToHash("idle");
 
     private void Start()
     {
@@ -14,6 +18,12 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (!isAttacking)
+        {
+            anim.CrossFade(IdleAnimation, 0, 0);
+        }
+        
+        
         if (Input.GetKeyDown(KeyCode.J))
         {
             Slash();
@@ -22,6 +32,8 @@ public class PlayerController : MonoBehaviour
 
     void Slash()
     {
+        StartCoroutine(PlayAttackAnimation());
+
         RaycastHit2D slashRadius = CheckSlashRadius();
 
         if (slashRadius.collider != null)
@@ -32,6 +44,15 @@ public class PlayerController : MonoBehaviour
 
     RaycastHit2D CheckSlashRadius()
     {
-        return Physics2D.CircleCast(_col.bounds.size, 2, Vector2.right, 0, projectileLayer);
+        return Physics2D.CircleCast(_col.bounds.size, 1.5f, Vector2.right, 0, projectileLayer);
+    }
+
+    IEnumerator PlayAttackAnimation()
+    {
+        isAttacking = true;
+        anim.CrossFade(SlashAnimation, 0, 0);
+        yield return new WaitForSeconds(.5f);
+        isAttacking = false;
+
     }
 }
